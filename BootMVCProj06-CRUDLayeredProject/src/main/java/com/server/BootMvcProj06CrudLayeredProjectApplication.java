c8e632f5ad3250709e2135d143bd92f9a43e8bf6
gityogeshsingh <40148123+gityogeshsingh@users.noreply.github.com>
2022-11-01 23:08:28 +0530

    First commit.

--- BootMVCProj06-CRUDLayeredProject/src/main/java/com/server/BootMvcProj06CrudLayeredProjectApplication.java
+++ BootMVCProj06-CRUDLayeredProject/src/main/java/com/server/BootMvcProj06CrudLayeredProjectApplication.java
@@ -3,7 +3,7 @@ package com.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+//this is execution point.
 @SpringBootApplication
 public class BootMvcProj06CrudLayeredProjectApplication {
 
