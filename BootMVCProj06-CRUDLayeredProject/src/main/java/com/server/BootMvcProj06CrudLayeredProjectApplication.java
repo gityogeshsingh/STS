70471b230903f683e85d05c0066057bd791aa808
gityogeshsingh <40148123+gityogeshsingh@users.noreply.github.com>
2022-11-01 23:12:52 +0530

    third

--- BootMVCProj06-CRUDLayeredProject/src/main/java/com/server/BootMvcProj06CrudLayeredProjectApplication.java
+++ BootMVCProj06-CRUDLayeredProject/src/main/java/com/server/BootMvcProj06CrudLayeredProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication //search for this
 public class BootMvcProj06CrudLayeredProjectApplication {
 
 	public static void main(String[] args) {
